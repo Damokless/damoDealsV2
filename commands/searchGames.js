a7e957d7c2e025df5b0b4bcc50a428558265a60c
Damokless <damokless@protonmail.com>
2022-11-30 11:12:09 +0100

    improve searchGames command

--- commands/searchGames.js
+++ commands/searchGames.js
@@ -1,6 +1,12 @@
 import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
+import updateConfigFile from './updateConfigFile.js';
+import config from '../config.json' assert {type: 'json'} ;
+
+const todayDate = new Date();
+let totalDeals = 0
 
 function sendEmbed(deal, interaction) {
+  totalDeals += 1
   const exampleEmbed = new EmbedBuilder()
     .setColor(0x0099FF)
     .setTitle(deal.title)
@@ -16,21 +22,26 @@ function sendEmbed(deal, interaction) {
     .setTimestamp()
     .setFooter({ text: 'Made with ❤️ by Damokles', iconURL: 'https://avatars.githubusercontent.com/u/66098929?v=4' });
 
-  interaction.channel.send({ embeds: [exampleEmbed] });
+  config.games ? interaction.client.channels.cache.get(config.games).send({ embeds: [exampleEmbed] }) : interaction.channel.send({ embeds: [exampleEmbed] })
 }
 
 const searchGames = {
   data: new SlashCommandBuilder()
     .setName('searchgames')
     .setDescription('search free games for all plateforms'),
-  async execute(interaction) {
-    await interaction.reply('Pong!');
+  async execute(interaction) { 
     const response = await fetch('https://www.gamerpower.com/api/giveaways?type=game', { method: 'GET' });
     const deals = await response.json();
     // eslint-disable-next-line no-restricted-syntax
     for (const deal of deals) {
-      sendEmbed(deal, interaction);
+      if (deal.published_date > config.lastUpdateGames) {
+        sendEmbed(deal, interaction);
+      }
     }
+    totalDeals === 0 ? await interaction.reply(`Pas de nouveaux deals depuis la dernière utilisation de la commande ${config.lastUpdateGames}`) : await interaction.reply(`J'ai trouvé ${totalDeals} jeux actuellement gratuits`)
+    config.lastUpdateGames = todayDate.toISOString().replace('T', ' ').slice(0, -5)
+    updateConfigFile(JSON.stringify(config))
+    totalDeals = 0
   },
 };
 
