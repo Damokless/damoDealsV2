c609d432a1e141e79c32ae650f4f8255979e61cd
Damokless <damokless@protonmail.com>
2022-12-27 12:06:03 +0100

    use update to write in db config

--- tools/updateConfigFile.js
+++ tools/updateConfigFile.js
@@ -8,5 +8,6 @@ const db = await open({
 
 async function updateConfigFile(key, data) {
   await db.run(`INSERT INTO config(${key}) VALUES (${data})`);
+  await db.run(`UPDATE config SET ${key} = ${data}`)
 }
 export default updateConfigFile;
